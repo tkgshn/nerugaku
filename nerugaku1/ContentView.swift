3a101dcfd83d29ee97bc8b8f78e88717853f458f
Shunsuke Takagi <takagishunsuke1129@gmail.com>
2020-07-21 17:01:09 +0900

    Initial Commit

---
+++ nerugaku1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nerugaku1
+//
+//  Created by Shunsuke Takagi on 2020/07/21.
+//  Copyright © 2020 Shunsuke Takagi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
